81da6e7a354c4d5910a8c96f7d884a8d3efe5fc1
Gytis Karčiauskas <gytis@karciauskas.lt>
2015-01-06 14:21:51 +0200

    Fixes.

--- src/ONGR/DemoBundle/DependencyInjection/Configuration.php
+++ src/ONGR/DemoBundle/DependencyInjection/Configuration.php
@@ -25,7 +25,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ongr_demo');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
