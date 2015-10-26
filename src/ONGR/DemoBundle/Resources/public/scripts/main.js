fa2b7ddb880b320612c91f929ec3828e9bc77c95
Mantas Kaveckas <kaveckasm@gmail.com>
2015-10-26 13:27:33 +0000

    home page featured products design, translation fixes, main js refactored

--- src/ONGR/DemoBundle/Resources/public/scripts/main.js
+++ src/ONGR/DemoBundle/Resources/public/scripts/main.js
@@ -7,59 +7,91 @@
  * file that was distributed with this source code.
  */
 
-(function ($) {
-    var topbarDropdown = '.topbar-dropdown-content';
-    var navbarDropdown = '.navbar-dropdown-content';
+(function($) {
+    var ONGR = {
+        init: function() {
+            this.bindDropdown('.navbar-dropdown', '.navbar-dropdown-content', 'fade');
+            this.bindDropdown('.topbar-dropdown', '.topbar-dropdown-content', 'slide');
+            this.bindFooterDropdown('.js-footer-dropdown-trigger');
+            this.bindHeaderSearch('.page-header-search-inp', '.page-header-search-btn');
+            this.bindHeaderCart('.page-header-cart-close', '.topbar-dropdown-content');
+            this.bindFeaturedCarousel();
+        },
 
-    $('.topbar-dropdown')
-        .mouseenter(function() {
-            $(this).addClass('opened');
-            $(this).find(topbarDropdown).slideDown('fast');
-        })
-        .mouseleave(function() {
-            $(this).removeClass('opened');
-            $(this).find(topbarDropdown).slideUp('fast');
-        });
-
-    $('.navbar-dropdown')
-        .mouseenter(function() {
-            $(this).addClass('opened');
-            $(this).find(navbarDropdown).fadeIn('fast');
-        })
-        .mouseleave(function() {
-            $(this).removeClass('opened');
-            $(this).find(navbarDropdown).fadeOut('fast');
-        });
-
-    $('.js-footer-dropdown-trigger').click(function() {
-        var trigger = $(this);
-
-        if ($(this).hasClass('opened')) {
-            $(this).next().slideUp('fast', function() {
-                trigger.removeClass('opened');
+        /**
+         *
+         * @param trigger
+         * @param target
+         * @param type
+         */
+        bindDropdown: function(trigger, target, type) {
+            $(trigger).hover(function() {
+                $(this).addClass('opened');
+                (type === 'fade') ? $(this).find(target).stop(true).delay(300).fadeIn('fast') : $(this).find(target).stop(true).delay(300).slideDown('fast');
+            }, function() {
+                $(this).removeClass('opened');
+                (type === 'fade') ? $(this).find(target).fadeOut('fast') : $(this).find(target).slideUp('fast');
             });
-        } else {
-            trigger.addClass('opened');
-            $(this).next().slideDown('fast');
+        },
+
+        /**
+         *
+         * @param target
+         */
+        bindFooterDropdown: function(target) {
+            $(target).click(function() {
+                var $trigger = $(this);
+                if ($(this).hasClass('opened')) {
+                    $(this).next().slideUp('fast', function() {
+                        $trigger.removeClass('opened');
+                    });
+                } else {
+                    $trigger.addClass('opened');
+                    $(this).next().slideDown('fast');
+                }
+            });
+        },
+
+        /**
+         *
+         * @param input
+         * @param button
+         */
+        bindHeaderSearch: function(input, button) {
+            $(input).click(function(e) {
+                $(this).parent().width('300px');
+                e.stopPropagation();
+            });
+
+            $(document).click(function() {
+                $(input).parent().width('200px');
+            });
+
+            $(button).click(function(e) {
+                e.stopPropagation();
+                $(this).parent().submit();
+            });
+        },
+
+        /**
+         *
+         * @param trigger
+         * @param target
+         */
+        bindHeaderCart: function(trigger, target) {
+            $(trigger).click(function(e) {
+                e.preventDefault();
+                $(this).parents().find(target).slideUp('fast');
+            });
+        },
+
+        bindFeaturedCarousel: function() {
+            $('#featured-carousel').carousel({
+                interval: 3000
+            });
         }
-    });
+    };
 
-    $('.page-header-search-inp').click(function(e) {
-        $(this).parent().width('300px');
-        e.stopPropagation();
-    });
-
-    $(document).click(function() {
-        $('.page-header-search-inp').parent().width('200px');
-    });
-
-    $('.page-header-search-btn').click(function(e) {
-        e.stopPropagation();
-        $(this).parent().submit();
-    });
-
-    $('.page-header-cart-close').click(function(e) {
-        e.preventDefault();
-        $(this).parents().find(topbarDropdown).slideUp('fast');
-    });
+    var App = Object.create(ONGR); App.init();
 })(jQuery);
+
